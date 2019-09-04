dd5e4f20061556e7aed0eb6ca6af93fa770230a6
PavelGnatyuk <pgnatyuk@gmail.com>
2019-09-04 08:40:08 +0300

    added extension for CustomDebugStringConvertible protocol with default debugDescription implementation

---
+++ Sources/TheTools/CustomDebugStringConvertible+debugDescription.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pavel Gnatyuk on 04/09/2019.
+//
+
+import Foundation
+
+public extension CustomDebugStringConvertible {
+    var debugDescription: String {
+        let className = type(of: self)
+        let address = "\(Unmanaged.passUnretained(self as AnyObject).toOpaque())"
+        let description = "<\(className): \(address)>"
+        return description
+    }
+}
