ac8e34688b10c2d716cc6b187e72245de501593a
PavelGnatyuk <pgnatyuk@gmail.com>
2019-11-10 12:05:46 +0200

    UIApplication extension added. Method getTopViewController

---
+++ Sources/TheTools/UIApplication+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pavel Gnatyuk on 10/11/2019.
+//
+
+import UIKit
+
+extension UIApplication {
+    
+    class func getTopViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return getTopViewController(base: nav.visibleViewController)
+            
+        } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return getTopViewController(base: selected)
+            
+        } else if let presented = base?.presentedViewController {
+            return getTopViewController(base: presented)
+        }
+        return base
+    }
+}
