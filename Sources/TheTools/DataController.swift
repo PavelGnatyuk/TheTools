cbdb5c2dde980e148c1ef9b7cecf296a9369df6e
PavelGnatyuk <pgnatyuk@gmail.com>
2019-12-21 17:56:33 +0200

    Data Controller added

---
+++ Sources/TheTools/DataController.swift
@@ -0,0 +1,91 @@
+//
+//  DataController.swift
+//
+//  ## References:
+//  1. [Core Data and App Extensions: Sharing a Single Database](https://medium.com/better-programming/core-data-and-app-extensions-sharing-a-single-database-24f8cac35faa)
+//  2. [Core Data Debugging in Xcode using launch arguments](https://www.avanderlee.com/debugging/core-data-debugging-xcode/)
+//
+//
+//  Created by Pavel Gnatyuk on 29/06/2019.
+//  Copyright © 2019 Pavel Gnatyuk. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class DataController {
+    public let dataFolder: URL?
+    public let bundle: Bundle
+    public let modelName: String
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        return NSManagedObjectModel.mergedModel(from: [self.bundle])!
+    }()
+
+    lazy var persistentContainer: NSPersistentContainer = makePersistentContainer()
+    
+    public lazy var viewContext: NSManagedObjectContext = {
+        let context = self.persistentContainer.viewContext
+        // From "Cracking the Tests for Core Data":
+        // https://medium.com/flawless-app-stories/cracking-the-tests-for-core-data-15ef893a3fee
+        // and from "Easier Core Data Setup with Persistent Containers":
+        // https://useyourloaf.com/blog/easier-core-data-setup-with-persistent-containers/
+        context.automaticallyMergesChangesFromParent = true
+        return context
+    }()
+    
+    /// Initialize the Core Data stack
+    /// - Parameters:
+    ///   - bundle: Bunlde where the data model is located
+    ///   - dataFolder: folder where the data itself is located
+    public init(modelName: String, bundle: Bundle, dataFolder: URL? = nil) {
+        self.modelName = modelName
+        self.bundle = bundle
+        self.dataFolder = dataFolder
+    }
+    
+    public func saveContext() {
+        let context = self.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                
+            } catch let error as NSError {
+                // Added from a Florian book Core Data. It simply abandons the unsaved data.
+                context.rollback()
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
+
+
+extension DataController {
+    func makePersistentContainer() -> NSPersistentContainer {
+        let container = NSPersistentContainer(name: modelName, managedObjectModel: managedObjectModel)
+        // `persistentStoreDescriptions` of the container by default is an array of one element - a path to the 'Library/Application Support/lazyness.sqlite'
+        container.persistentStoreDescriptions = updateIfNeeded(descriptions: container.persistentStoreDescriptions)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }
+
+    func updateIfNeeded(descriptions: [NSPersistentStoreDescription]) -> [NSPersistentStoreDescription] {
+        guard let storeURL = dataFolder, let url = descriptions.first?.url else {
+            return descriptions
+        }
+        let path = storeURL.appendingPathComponent(url.lastPathComponent)
+        return [NSPersistentStoreDescription(url: path)]
+    }
+}
+
+extension DataController: CustomStringConvertible {
+    public var description: String {
+        let text = "\(DataController.self) model name = \(modelName) data folder = \(String(describing: dataFolder))"
+        return text
+    }
+}
+
---
+++ Tests/TheToolsTests/DataControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  DataControllerTests.swift
+//  TheToolsTests
+//
+//  Created by Pavel Gnatyuk on 21/12/2019.
+//
+
+import XCTest
+@testable import TheTools
+
+class DataControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testThatDataCOntrollerCanBeCreated() {
+        let bundle = Bundle(for: type(of: self))
+        let controller = DataController(modelName: "", bundle: bundle)
+        XCTAssertNotNil(controller, "Failed to create a dummy Data Controller")
+        XCTAssertEqual(controller.modelName, "", "Model name in the created Data controller instance should be empty string")
+    }
+
+    func testThatDataCOntrollerCanBeCreatedWithModelName() {
+        let modelName = "model"
+        let bundle = Bundle(for: type(of: self))
+        let controller = DataController(modelName: modelName, bundle: bundle)
+        XCTAssertNotNil(controller, "Failed to create a dummy Data Controller")
+        XCTAssertEqual(controller.modelName, modelName, "Model name in the created Data controller instance does not equal to the input parameter")
+    }
+
+}
